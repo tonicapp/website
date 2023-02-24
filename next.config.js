34964b1e7cfcc9ff61e8232ed578cac70bffbbe9
Adithya Chandrasekar <21148381+adithyachan@users.noreply.github.com>
2023-02-24 18:10:28 -0500

    Update #1: Commit #5 - TailwindCSS Init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
   reactStrictMode: true,
 }
-
-module.exports = nextConfig
