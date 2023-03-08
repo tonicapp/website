239f0f20fc4d90cde2232147dbba881c59bcf994
Grant Rivera <80211407+grantrivera@users.noreply.github.com>
2023-03-08 06:33:36 -0500

    Initial amplify setup for hosting, basic API & lambda function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api32bc0a2f": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "LibraryViewer": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
